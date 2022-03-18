70fc599de4c1e0f76aaf7a617a851c59334c2df4
Grant Matthews <grant.d.matthews@gmail.com>
2022-03-18 00:19:50 -0500

    Adding GUI to main menu
    Still needs most modes implemented, classic creates a game currently

--- Wordle-Game/main.py
+++ Wordle-Game/main.py
@@ -3,27 +3,42 @@
 
 import random
 import json
+from tkinter import *
+from tkinter.ttk import *
 
-class game():
-    def __init__(self, word, num_guesses):
-        self.word = word
-        self.num_guesses = num_guesses
-
-    def guess(self, guess):
-        # return something to indicate guess
-        return()
-
+from game import *
 
 def classic():
+
+    #finding word
     filepath = "words/words-5.json"
     with open(filepath) as words_json:
         words = json.load(words_json)
         word = random.choice(list(words.keys()))
-        print(word)
-    return(game(word, 6))
+
+    game = Game(word, 6);
+    game.start_game()
 
 
 def main():
+    gui = Tk()
+    gui.title("Auto-Wordle")
+    gui.geometry("600x200")
+    gui.minsize(600,200)
+    gui.maxsize(600,200)
+
+    title_banner = Label(gui, text = "Welcome to Auto Wordle", font = ("Arial", 25)).place(x=115, y=25)
+    info_banner = Label(gui, text = "To begin, please select an option below. For more information, select help.").place(x=100, y=75)
+
+    classic_button = Button(gui, text = "Classic Mode", command = classic).place(bordermode=OUTSIDE, height=50, width=100, x=25, y=100)
+    random_button = Button(gui, text = "Random Mode").place(bordermode=OUTSIDE, height=50, width=100, x=150, y=100)
+    Custom_button = Button(gui, text = "Custom Mode").place(bordermode=OUTSIDE, height=50, width=100, x=275, y=100)
+
+    help_button = Button(gui, text = "Help").place(bordermode=OUTSIDE, height=25, width=50, x=275, y=175)
+
+    gui.mainloop()
+
+def main2():
     print("Select Mode: \nClassic Wordle - 1\nRandom Wordle - 2\nSet Parameters - 3")
     mode = int(input("Input:"))
     if(mode == 1):
