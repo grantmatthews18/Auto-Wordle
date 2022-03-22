f85dbd0bd417398d8332254a559555524e171f88
Grant Matthews <grant.d.matthews@gmail.com>
2022-03-22 11:33:39 -0500

    Update simularity-calc.py
    Beginning to create the script that returns similarity values for words

--- Words/simularity-calc.py
+++ Words/simularity-calc.py
@@ -1,12 +1,20 @@
 
 import json
+import copy
 
 with open("Words/wordle-words.json","r+") as words_json:
     words = json.load(words_json)
     for word in words.keys():
 
-        #normalized hamming distance
-        for i in range(len(word)):
-            if(word[i])
+        words[word] = [len(word),0,0,0,0,0]
 
-        words[word] = [len(word),0,0,0]
+        temp_words = copy.deepcopy(words.keys())
+        for temp_word in temp_words:
+            temp_word_arr = [char for char in temp_word]
+            matching_ct = 0
+            for char in word:
+                if char in temp_word_arr:
+                    matching_ct += 1
+                    temp_word_arr[temp_word_arr.index(char)] = None
+
+            #update word dict value depemdng on matching_ct
