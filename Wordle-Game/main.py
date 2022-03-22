cab88e07652b3d85b9eacb6cc6e82115091c341e
Grant Matthews <grant.d.matthews@gmail.com>
2022-03-21 21:54:34 -0500

    Update main.py
    Modifying for testing purposes

--- Wordle-Game/main.py
+++ Wordle-Game/main.py
@@ -3,15 +3,15 @@
 
 import random
 import json
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+import tkinter.ttk as ttk
 
 from game import *
 
 def classic():
 
     #finding word
-    filepath = "words/words-5.json"
+    filepath = "Words/wordle-words.json"
     with open(filepath) as words_json:
         words = json.load(words_json)
         word = random.choice(list(words.keys()))
@@ -21,20 +21,20 @@ def classic():
 
 
 def main():
-    gui = Tk()
+    gui = tk.Tk()
     gui.title("Auto-Wordle")
     gui.geometry("600x200")
     gui.minsize(600,200)
     gui.maxsize(600,200)
 
-    title_banner = Label(gui, text = "Welcome to Auto Wordle", font = ("Arial", 25)).place(x=115, y=25)
-    info_banner = Label(gui, text = "To begin, please select an option below. For more information, select help.").place(x=100, y=75)
+    title_banner = tk.Label(gui, text = "Welcome to Auto Wordle", font = ("Arial", 25)).place(x=115, y=25)
+    info_banner = tk.Label(gui, text = "To begin, please select an option below. For more information, select help.").place(x=100, y=75)
 
-    classic_button = Button(gui, text = "Classic Mode", command = classic).place(bordermode=OUTSIDE, height=50, width=100, x=25, y=100)
-    random_button = Button(gui, text = "Random Mode").place(bordermode=OUTSIDE, height=50, width=100, x=150, y=100)
-    Custom_button = Button(gui, text = "Custom Mode").place(bordermode=OUTSIDE, height=50, width=100, x=275, y=100)
+    classic_button = tk.Button(gui, text = "Classic Mode", command = classic).place(bordermode=tk.OUTSIDE, height=50, width=100, x=25, y=100)
+    random_button = tk.Button(gui, text = "Random Mode").place(bordermode=tk.OUTSIDE, height=50, width=100, x=150, y=100)
+    Custom_button = tk.Button(gui, text = "Custom Mode").place(bordermode=tk.OUTSIDE, height=50, width=100, x=275, y=100)
 
-    help_button = Button(gui, text = "Help").place(bordermode=OUTSIDE, height=25, width=50, x=275, y=175)
+    help_button = tk.Button(gui, text = "Help").place(bordermode=tk.OUTSIDE, height=25, width=50, x=275, y=175)
 
     gui.mainloop()
 
