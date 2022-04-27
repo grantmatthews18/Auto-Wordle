62fc639d4e540a8d661d00872b468b91e9f43f7f
Grant Matthews <grant.d.matthews@gmail.com>
2022-04-27 14:13:56 -0500

    Testing

--- Words/easytemp.py
+++ Words/easytemp.py
@@ -3,7 +3,7 @@ import copy
 from itertools import product
 import sys
 sys.path.insert(1, "AI/")
-import agent_constants as ac
+import constants as ac
 # with open("Words/wordle-words.json","r+") as words_json:
 #     words = json.load(words_json)
 
@@ -17,3 +17,8 @@ possible_result_arrs = [char for char in it_arr]
 #         text.write(str(element) + ", ")
 
 print(ac.POSSIBLE_RESULT_ARRAY[0])
+
+tuple1 = ("a",)
+tuple2 = ("b",)
+tuple3 = tuple1 + tuple2
+print(tuple3)
